646a4d6dd969c46dc5af46a4c6fdb4506e9de8f3
bilel <bilel.bekkouche@web-atrio.com>
2020-05-26 14:33:36 +0200

    seeder page

--- app/Models/Pages.php
+++ app/Models/Pages.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Spatie\Image\Exceptions\InvalidManipulation;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\Models\Media;
--- database/seeds/PagesSeeder.php
+++ database/seeds/PagesSeeder.php
@@ -17,22 +17,18 @@ class PagesSeeder extends Seeder
             [
                 'unique_name' => Str::slug(strtolower('Qui Sommes nous'), '-'),
                 'title' => 'Qui Sommes nous ?',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('NOS SERVICES'), '-'),
                 'title' => 'NOS SERVICES',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('COMMENT PRENDRE RENDEZ-VOUS'), '-'),
                 'title' => 'COMMENT PRENDRE RENDEZ-VOUS ?',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('TARIFS'), '-'),
                 'title' => 'Tarifs secteur 1 à titre indicatif au 1er Janvier 2020',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('OU SE SITUE LES MAISONS MÉDICALES DE GARDE'), '-'),
@@ -42,42 +38,34 @@ class PagesSeeder extends Seeder
             [
                 'unique_name' => Str::slug(strtolower('contacts'), '-'),
                 'title' => 'contacts',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('NOS INFORMATIONS'), '-'),
                 'title' => 'NOS INFORMATIONS',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('baniere homepage'), '-'),
                 'title' => 'baniere homepage',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('baniere qui somme nous'), '-'),
                 'title' => 'baniere qui somme nous',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('baniere tarifs'), '-'),
                 'title' => 'baniere tarifs',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('CE QUE LES DOCTEURS DE GARDE NE FONT PAS'), '-'),
                 'title' => 'CE QUE LES DOCTEURS DE GARDE® NE FONT PAS',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('UN RENDEZ-VOUS DANS QUEL DÉLAI'), '-'),
                 'title' => 'UN RENDEZ-VOUS DANS QUEL DÉLAI ?',
-                'content' => 'du lorem ispum',
             ],
             [
                 'unique_name' => Str::slug(strtolower('SUIS-JE REMBOURSÉ DES CONSULTATIONS'), '-'),
                 'title' => 'SUIS-JE REMBOURSÉ DES CONSULTATIONS ?',
-                'content' => 'du lorem ispum',
             ],
         ];
 
