00c92844c71033600e18aaa93a802340d008b6ef
bilel <bilel.bekkouche@web-atrio.com>
2020-06-05 22:56:04 +0200

    Clean du dépot

--- nova-components/Generatornewpassword/resources/js/field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting((Vue, router, store) => {
-    Vue.component('index-generatornewpassword', require('./components/IndexField'))
-    Vue.component('detail-generatornewpassword', require('./components/DetailField'))
-    Vue.component('form-generatornewpassword', require('./components/FormField'))
-})
